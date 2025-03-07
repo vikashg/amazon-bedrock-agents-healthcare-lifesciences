178d2890fd63a37ec454ca0b59e118579c3bf443
hasan poonawala <hasanp1987@gmail.com>
2025-03-07 14:01:04 +0000

    docs for github io

---
+++ docs/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  integrations: [starlight({
+    title: 'Amazon Bedrock Agents for Healthcare and LifeSciences',
+    social: {
+      github: 'https://github.com/aws-samples/amazon-bedrock-agents-cancer-biomarker-discovery/tree/multi-agent-collaboration',
+    },
+    sidebar: [
+      {
+        label: 'Getting Started',
+        items: [
+          { label: 'Introduction', link: '/' },
+          { label: 'Setup', link: '/setup/' },
+        ]
+      },
+      {
+        label: 'Components',
+        items: [
+          { label: 'Agents Catalog', link: '/agents_catalog/' },
+          { label: 'Multi-Agent Orchestration', link: '/multi_agent_orchestration/' },
+          { label: 'Deployment', link: '/deployment/' },
+          { label: 'Evaluations', link: '/evaluations/' },
+        ]
+      },
+    ],
+  }), mdx()],
+});
