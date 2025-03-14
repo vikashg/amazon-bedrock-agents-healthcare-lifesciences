a96c1c8fdddfeb2c0febd70df08069660928fec2
hasan poonawala <hasanp1987@gmail.com>
2025-03-14 12:36:09 +0000

    doc update

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://aws-samples.github.io',
-  base: '/amazon-bedrock-agents-healthcare-lifesciences',
+  base: '/amazon-bedrock-agents-healthcare-lifesciences/',
   trailingSlash: 'always',
   integrations: [starlight({
     title: 'Amazon Bedrock Agents for Healthcare and LifeSciences',
