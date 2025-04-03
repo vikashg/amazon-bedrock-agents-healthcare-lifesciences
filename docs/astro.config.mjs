dfd7919b0eaeff14e3a9e61daaa0ce11cdae472e
hasan poonawala <hasanp1987@gmail.com>
2025-04-03 12:58:29 +0100

    update landing page

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,23 +10,23 @@ export default defineConfig({
   integrations: [starlight({
     title: 'Amazon Bedrock Agents for Healthcare and LifeSciences',
     social: {
-      github: 'https://github.com/aws-samples/amazon-bedrock-agents-cancer-biomarker-discovery/tree/multi-agent-collaboration',
+      github: 'https://github.com/aws-samples/amazon-bedrock-agents-healthcare-lifesciences/tree/main',
     },
     sidebar: [
       {
         label: 'Getting Started',
         items: [
           { label: 'Introduction', link: '/' },
-          { label: 'Setup', link: '/setup/' },
+          { label: 'GitHub', link: 'https://github.com/aws-samples/amazon-bedrock-agents-healthcare-lifesciences/tree/main' },
         ]
       },
       {
         label: 'Components',
         items: [
-          { label: 'Agents Catalog', link: './agents_catalog/' },
-          { label: 'Multi-Agent Orchestration', link: './multi_agent_orchestration/' },
-          { label: 'Deployment', link: './deployment/' },
-          { label: 'Evaluations', link: './evaluations/' },
+          { label: 'Agents Catalog', link: 'https://github.com/aws-samples/amazon-bedrock-agents-healthcare-lifesciences/tree/main/agents_catalog' },
+          { label: 'Multi-Agent Orchestration', link: 'https://github.com/aws-samples/amazon-bedrock-agents-healthcare-lifesciences/tree/main/multi_agent_collaboration/' },
+          { label: 'Deployment', link: 'https://github.com/aws-samples/amazon-bedrock-agents-healthcare-lifesciences/tree/main?tab=readme-ov-file#deployment' },
+          { label: 'Evaluations', link: 'https://github.com/aws-samples/amazon-bedrock-agents-healthcare-lifesciences/tree/main/evaluations' },
         ]
       },
     ],
